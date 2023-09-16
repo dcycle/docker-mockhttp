51ff67949fd8b5e007ea72c682eedbf43b45e48b
Albert Albala <albert@mediatribe.net>
2023-09-16 10:42:54 -0400

    Please squash this commit in the pull request

--- scripts/test.sh
+++ scripts/test.sh
@@ -11,3 +11,11 @@ set -e
 
 docker pull php:apache
 docker build -t local-mockhttp .
+
+docker run --rm local-mockhttp /bin/bash -c 'ls -lah'
+
+docker run --rm local-mockhttp /bin/bash -c 'ls -lah' | grep '.htaccess'
+
+echo ""
+echo "All tests are passing."
+echo ""
